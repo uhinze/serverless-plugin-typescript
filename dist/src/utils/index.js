1727a2849e739750e9b26a1c71484094ccb20aa3
edraitsev <Eugene.Draitsev@ebuilder.com>
2018-05-13 11:46:34 +0200

    Version with committed build folder, to allow use plugin from npm

---
+++ dist/src/utils/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs-extra");
+const isMissingSymlinkPermission = (error) => {
+    // Generally happens when no admin rights with UAC enabled on Windows.
+    return error.code === 'EPERM' && error.errno === -4048;
+};
+const copyIfMissingSymlinkPermission = (srcpath, dstpath, error) => {
+    if (this.isMissingSymlinkPermission(error)) {
+        fs.copySync(srcpath, dstpath);
+    }
+    else {
+        throw error;
+    }
+};
+exports.symlink = (srcpath, dstpath, type) => {
+    try {
+        fs.symlinkSync(srcpath, dstpath, type);
+    }
+    catch (error) {
+        this.copyIfMissingSymlinkPermission(srcpath, dstpath, error);
+    }
+};
+//# sourceMappingURL=index.js.map
